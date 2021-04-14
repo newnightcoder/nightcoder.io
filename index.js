b32eccbab349bcbc8c61898fcb24cdfb2fe9b44d
Nightcoder <newnightcoder@gmail.com>
2021-04-14 07:02:54 +0200

    navbar/flags fixed :wrench:

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ logo.forEach((log, i) => console.log(`${i + 1}: ${log.getTotalLength()}`));
 
 const navbar = document.querySelector("nav");
 const flags = document.querySelector(".flags-container");
+
 window.addEventListener("scroll", () => {
   window.scrollY >= window.innerHeight / 4
-    ? ((navbar.style.backgroundColor = "#0037ad"),
-      (flags.style.opacity = ".75"))
+    ? ((navbar.style.backgroundColor = "#1e1e1e"),
+      (navbar.style.borderBottom = "1px solid #3e3e42"),
+      (flags.style.opacity = "1"))
     : ((navbar.style.backgroundColor = "transparent"),
+      (navbar.style.borderBottom = "1px solid transparent"),
       (flags.style.opacity = "0"));
 });
