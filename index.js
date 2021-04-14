59c80077e568a03d13dc82addb894b2d5d5eafd9
Nightcoder <newnightcoder@gmail.com>
2021-04-14 09:40:29 +0200

    more design details :wrench:

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const flags = document.querySelector(".flags-container");
 window.addEventListener("scroll", () => {
   window.scrollY >= window.innerHeight / 4
     ? ((navbar.style.background =
-        "linear-gradient(to right, #3e3e42, #1e1e1e)"),
-      (navbar.style.borderBottom = "1px solid lightgray"),
+        "linear-gradient(to left, deeppink 0%, orange 100%)"),
+      // (navbar.style.borderBottom = "1px solid lightgray")
       (flags.style.opacity = "1"),
       (navbarLinks.style.transform = "translateY(0)"))
     : ((navbar.style.background = "transparent"),
-      (navbar.style.borderBottom = "1px solid transparent"),
+      // (navbar.style.borderBottom = "1px solid transparent")
       (flags.style.opacity = "0"),
       (navbarLinks.style.transform = "translateY(1.75vh)"));
 });
