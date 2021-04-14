194e65c2b8abd4f59efa8d39dcb175d4656f7651
Nightcoder <newnightcoder@gmail.com>
2021-04-14 07:30:53 +0200

    design details :wrench:

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ console.log(logo);
 logo.forEach((log, i) => console.log(`${i + 1}: ${log.getTotalLength()}`));
 
 const navbar = document.querySelector("nav");
+const navbarLinks = document.querySelector(".link-wrapper");
 const flags = document.querySelector(".flags-container");
 
 window.addEventListener("scroll", () => {
   window.scrollY >= window.innerHeight / 4
-    ? ((navbar.style.backgroundColor = "#1e1e1e"),
-      (navbar.style.borderBottom = "1px solid #3e3e42"),
-      (flags.style.opacity = "1"))
-    : ((navbar.style.backgroundColor = "transparent"),
+    ? ((navbar.style.background =
+        "linear-gradient(to right, #3e3e42, #1e1e1e)"),
+      (navbar.style.borderBottom = "1px solid lightgray"),
+      (flags.style.opacity = "1"),
+      (navbarLinks.style.transform = "translateY(0)"))
+    : ((navbar.style.background = "transparent"),
       (navbar.style.borderBottom = "1px solid transparent"),
-      (flags.style.opacity = "0"));
+      (flags.style.opacity = "0"),
+      (navbarLinks.style.transform = "translateY(2vh)"));
 });
