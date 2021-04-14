dcb48a85902e6f61450c035319b89bf1fe7bf911
Nightcoder <newnightcoder@gmail.com>
2021-04-14 15:28:01 +0200

    navbar improvement/brand-name :wrench:

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ logo.forEach((log, i) => console.log(`${i + 1}: ${log.getTotalLength()}`));
 const navbar = document.querySelector("nav");
 const navbarLinks = document.querySelector(".link-wrapper");
 const flags = document.querySelector(".flags-container");
+const brand = document.querySelector(".brand-name");
 
 window.addEventListener("scroll", () => {
-  window.scrollY >= window.innerHeight / 4
-    ? ((navbar.style.background =
-        "linear-gradient(to left, deeppink 0%, orange 100%)"),
-      // (navbar.style.borderBottom = "1px solid lightgray")
+  if (window.scrollY >= window.innerHeight / 4) {
+    (navbar.style.background =
+      "linear-gradient(to left, deeppink 0%, orange 100%)"),
       (flags.style.opacity = "1"),
-      (navbarLinks.style.transform = "translateY(0)"))
-    : ((navbar.style.background = "transparent"),
-      // (navbar.style.borderBottom = "1px solid transparent")
+      (navbarLinks.style.transform = "translateY(0)");
+  } else {
+    (navbar.style.background = "transparent"),
       (flags.style.opacity = "0"),
-      (navbarLinks.style.transform = "translateY(1.75vh)"));
+      (navbarLinks.style.transform = "translateY(1.75vh)");
+  }
+  if (window.scrollY >= window.innerHeight / 0.9) {
+    brand.style.opacity = "1";
+  } else {
+    brand.style.opacity = "0";
+  }
 });
