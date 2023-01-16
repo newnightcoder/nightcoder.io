2ef81c5f2797ae43e8b8b6ea41057eb9eb696605
Nightcoder <newnightcoder@gmail.com>
2023-01-16 21:51:44 +0100

    animation transition between slug pages fixed :rocket:

--- animations/HomeAnimation.tsx
+++ animations/HomeAnimation.tsx
@@ -1,5 +1,5 @@
 import gsap from "gsap";
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import { TransitionContext } from "../context/TransitionContext";
 import { useIsoMorphicLayoutEffect } from "../hooks";
 import { AnimationContainer } from "../styles/home";
@@ -8,8 +8,18 @@ const HomeAnimation = ({ children }) => {
   const { timelinePages, isMenuOpen } = useContext(TransitionContext);
   const spanref = useRef<HTMLDivElement>(null);
 
+  const [isAnimDone, setIsAnimDone] = useState(true);
+
   useIsoMorphicLayoutEffect(() => {
-    if (spanref.current && !isMenuOpen) {
+    if (spanref.current && !isMenuOpen && isAnimDone) {
+      // re-set element's position at each remount
+      gsap.set(spanref.current, {
+        autoAlpha: 0,
+        y: 100,
+        x: 0,
+      });
+
+      // enter animation
       gsap.to(spanref.current, {
         autoAlpha: 1,
         y: 0,
@@ -17,17 +27,19 @@ const HomeAnimation = ({ children }) => {
         delay: 0.5,
       });
 
-      // out animation
+      // exit animation
       timelinePages.add(
         gsap.to(spanref.current, {
           x: -1000,
           autoAlpha: 0,
           duration: 1,
+          onStart: () => setIsAnimDone(false),
+          onComplete: () => setIsAnimDone(true),
         }),
         0
       );
     }
-  }, [isMenuOpen]);
+  }, [isMenuOpen, isAnimDone]);
 
   return <AnimationContainer ref={spanref}>{children}</AnimationContainer>;
 };
